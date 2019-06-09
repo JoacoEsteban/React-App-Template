0849ff1375a3cd686cd319db5eef470b80187963
JoacoEsteban <dev.joaco.esteban@gmail.com>
2019-06-09 01:46:18 -0300

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+import { Animate } from "react-animate-mount";
+
+class App extends Component
+{
+    
+    render()
+    {
+        return(
+            <div>
+                React App
+            </div>
+        )
+    }
+}
+
+
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import './styles.css'  
+
+ReactDOM.render(<App />, document.getElementById('root')
+
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Animal
+// {
+//     constructor(name, age)
+//     {
+//         this.name = name;
+//         this.age = age;
+//     }
+
+//     speak()
+//     {
+//         console.log('I am ', this.name, 'and I am', this.age, 'Years Old.');
+//     }
+// }
+
+// const animal1 = new Animal('Simba', 3);
+
+
+// class Lion extends Animal
+// {
+//     constructor(name, age, furColor, speed)
+//     {
+//         super(name, age);
+//         this.furColor = furColor;
+//         this.speed = speed;
+//     }
+//     roar()
+//     {
+//         console.log('ROAR BICH, IM COLOR', this.furColor, 'AND I GO', this.speed, 'KM POR HORA' );
+//     }
+// }
+
+// const lion1 = new Lion( 'Mufasa xd', 43, 'golden', 900);
